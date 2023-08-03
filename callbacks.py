1e173b0d67d9ef5020199f67f899ca316e558fb8
yuval <yuvalgabai96@gmail.com>
2023-08-03 17:59:26 +0300

    merge with legs movement, rock bounds and rock+fence into general objects

--- callbacks.py
+++ callbacks.py
@@ -4,12 +4,10 @@ from OpenGL.GLU import *
 from OpenGL.GLUT import *
 import numpy as np
 import math
-from fence import draw_fence, NUM_PARTS, CHANGE
 from cow import cow
-from general_objects import draw_grass, draw_lightpost, draw_sun
+from general_objects import draw_grass, draw_lightpost, draw_sun, draw_fence, draw_rocks_and_sword
 from light import setup_lighting, updateLight, set_matte_properties, set_shiny_properties, spot_rotate
-from utils import rotation_matrix_x, rotation_matrix_y, translation_matrix
-from rock import draw_rocks_and_sword
+from utils import rotation_matrix_x, rotation_matrix_y, translation_matrix 
 from menu import createMainMenu
 from globals import *
 
@@ -37,8 +35,9 @@ def init():
 
 # Display callback function that repeatedly renders what's on the screen.
 def myDisplay():
-    global angle, winH, head_angle_l_r, head_angle_u_d, head_up_vector, left_legs_angle, right_legs_angle
-    global tail_angle_l_r, tail_angle_u_d, body_loc, body_move, x_fence, cow_len_z
+    global angle, winH, head_angle_l_r, head_angle_u_d, head_up_vector, left_legs_angle_u_d 
+    global right_legs_angle_u_d, legs_angle_l_r, tail_angle_l_r, tail_angle_u_d
+    global body_loc, body_move, x_fence, cow_len_z
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     #render matte objects
     set_matte_properties()
@@ -65,14 +64,17 @@ def myDisplay():
     glTranslate(body_move[0],y,body_move[1])
     glRotatef(body_angle, 0, 1, 0)  
     glTranslate(-x,-y,-z)  
-    cow(x, z, cow_len_z, head_angle_l_r, head_angle_u_d, tail_angle_l_r, tail_angle_u_d, left_legs_angle, right_legs_angle)
+    cow(x, z, cow_len_z, head_angle_l_r, head_angle_u_d, tail_angle_l_r, tail_angle_u_d, 
+        left_legs_angle_u_d, right_legs_angle_u_d, legs_angle_l_r)
     glPopMatrix()
 
     body_loc = body_move
 
-    #update legs when not moving
-    left_legs_angle = 0
-    right_legs_angle = 0
+    # update legs when not moving
+    if part_of_body != "legs":
+        left_legs_angle_u_d = 0
+        right_legs_angle_u_d = 0
+        legs_angle_l_r = 0
 
     glutSwapBuffers()   
 
@@ -163,6 +165,9 @@ def keyboard(key, x, y):
     
     elif (key == b'h' or key == b'H' ): 
         part_of_body = "head"
+    
+    elif key == b'f' or key == b'F':
+        part_of_body = "legs"
 
     elif (key == b'c' or key == b'C'):
         part_of_body = "camera"
@@ -184,16 +189,36 @@ def keyboard(key, x, y):
 # Some objects have more functionality.
 def move(key):
     global head_angle_l_r, head_angle_u_d, head_up_vector, part_of_body, body_loc, body_move, last_leg
-    global tail_angle_l_r, tail_angle_u_d, body_angle, left_legs_angle, right_legs_angle, x_fence, cow_len_z
-    global eyeX, eyeY, eyeZ, radius, camera_movement
+    global tail_angle_l_r, tail_angle_u_d, body_angle, left_legs_angle_u_d, right_legs_angle_u_d, legs_angle_l_r
+    global  x_fence, cow_len_z, eyeX, eyeY, eyeZ, radius, camera_movement
 
     # Explanation of cheking in body movement:
+
     # fence from (x_fence,0,0) to (x_fence + CHANGE*NUM_PARTS,0,0)
     bounds_z_up = (0 - (2/3)*cow_len_z, 0 + (2/3)*cow_len_z)
     # then from (x_fence + CHANGE*NUM_PARTS,0,0) to (x_fence + CHANGE*NUM_PARTS,0,CHANGE*NUM_PARTS)
     bounds_x = (x_fence + CHANGE*NUM_PARTS - (2/3)*cow_len_z, x_fence + CHANGE*NUM_PARTS + (2/3)*cow_len_z)
     # then from (x_fence + CHANGE*NUM_PARTS,0,CHANGE*NUM_PARTS) to (x_fence,0,CHANGE*NUM_PARTS)
     bounds_z_down = (-1*CHANGE*NUM_PARTS + (2/3)*cow_len_z, -1*CHANGE*NUM_PARTS - (2/3)*cow_len_z)
+
+    # rocks bounds
+    bounds_rock_center_x = (x_rock - gaps[0], x_rock + rock_bases[0] + gaps[0])
+    bounds_rock_center_z = (z_rock - rock_bases[0] - gaps[0], z_rock + gaps[0])
+
+    bounds_rock_left_x = (x_rock + rock_bases[0] - gaps[1], 
+                          x_rock + rock_bases[0] + rock_bases[1] + gaps[1])
+    bounds_rock_left_z = (z_rock - rock_bases[1] - gaps[1], z_rock + gaps[1])
+
+    bounds_rock_right_x = (x_rock - (4/5)*rock_bases[0] - gaps[2], x_rock - (4/5)*rock_bases[0]
+                           + rock_bases[2] + gaps[2])
+    bounds_rock_right_z = (z_rock - (1/2)*rock_bases[0] - rock_bases[2] - gaps[2], 
+                           z_rock - (1/2)*rock_bases[0] + gaps[2])
+    
+    bounds_rock_behind_x = (x_rock - gaps[3], x_rock + rock_bases[3] + gaps[3])
+    bounds_rock_behind_z = (z_rock - gaps[3], z_rock + rock_bases[3] + gaps[3])
+
+    bounds_rocks_x = [bounds_rock_center_x, bounds_rock_left_x, bounds_rock_right_x, bounds_rock_behind_x]
+    bounds_rocks_z = [bounds_rock_center_z, bounds_rock_left_z, bounds_rock_right_z, bounds_rock_behind_z]
 
     if (key == b'j' or key == b'J'): # "Left movement" on the following objects.
 
@@ -214,6 +239,9 @@ def move(key):
 
         elif part_of_body == "tail" and tail_angle_l_r > -25:
             tail_angle_l_r -= 5
+
+        elif part_of_body == "legs" and legs_angle_l_r > -30:
+            legs_angle_l_r -= 5
 
         elif part_of_body == "camera": # setting of the camera left movement behavior.
             if eyeX == radius:
@@ -257,6 +285,9 @@ def move(key):
 
         elif part_of_body == "tail" and tail_angle_l_r < 25:
             tail_angle_l_r += 5
+
+        elif part_of_body == "legs" and legs_angle_l_r < 30:
+            legs_angle_l_r += 5
 
         elif part_of_body == "camera": # setting of the camera Right movement behavior.
             if eyeX == radius:
@@ -312,15 +343,23 @@ def move(key):
                 (z > bounds_z_down[0] >= body_move[1] or body_move[1] > bounds_z_down[1] >= z) and
                 x_fence + CHANGE*NUM_PARTS > x > x_fence):
                 body_move = body_loc
+
+            # rocks bounds
+            for br_x, br_z in zip(bounds_rocks_x, bounds_rocks_z):
+                if ((z <= br_z[0] - (2/3)*cow_len_z < body_move[1] or 
+                     body_move[1] < br_z[1] + (2/3)*cow_len_z <= z) and 
+                    br_x[0] < x < br_x[1]) or ((x <= br_x[0] - (2/3)*cow_len_z < body_move[0] or 
+                    body_move[0] < br_x[1] + (2/3)*cow_len_z <= x) and br_z[0] < z < br_z[1]):
+                    body_move = body_loc
              
             if last_leg == "left":
-                left_legs_angle = -10
-                right_legs_angle = 10
+                left_legs_angle_u_d = -10
+                right_legs_angle_u_d = 10
                 last_leg = "right"
 
             elif last_leg == "right":
-                left_legs_angle = 10
-                right_legs_angle = -10
+                left_legs_angle_u_d = 10
+                right_legs_angle_u_d = -10
                 last_leg = "left"
         
         elif part_of_body == "head" and head_angle_u_d < 15:
@@ -358,6 +397,7 @@ def move(key):
                 alpha = math.radians(body_angle - 270)
                 body_move = (x - 5*math.cos(alpha),z + 5*math.sin(alpha))
 
+            # fence bounds
             if ((z > bounds_z_up[1] >= body_move[1] or body_move[1] > bounds_z_up[0] >= z) and 
                 x_fence + CHANGE*NUM_PARTS > x > x_fence) or (
                 (x > bounds_x[1] >= body_move[0] or body_move[0] > bounds_x[0] >= x) and
@@ -366,15 +406,23 @@ def move(key):
                 x_fence + CHANGE*NUM_PARTS > x > x_fence):
                 body_move = body_loc
 
+            # rocks bounds
+            for br_x, br_z in zip(bounds_rocks_x, bounds_rocks_z):
+                if ((z <= br_z[0] - (2/3)*cow_len_z < body_move[1] or 
+                     body_move[1] <= br_z[1] + (2/3)*cow_len_z < z) and 
+                    br_x[0] < x < br_x[1]) or ((x <= br_x[0] - (2/3)*cow_len_z < body_move[0] or 
+                    body_move[0] <= br_x[1] + (2/3)*cow_len_z < x) and br_z[0] < z < br_z[1]):
+                    body_move = body_loc
+
             # Leg movement while moving back.
             if last_leg == "left":  
-                left_legs_angle = -10
-                right_legs_angle = 10
+                left_legs_angle_u_d = -10
+                right_legs_angle_u_d = 10
                 last_leg = "right"
 
             elif last_leg == "right":
-                left_legs_angle = 10
-                right_legs_angle = -10
+                left_legs_angle_u_d = 10
+                right_legs_angle_u_d = -10
                 last_leg = "left"
         
         elif part_of_body == "head" and head_angle_u_d > -30:
--- general_objects.py
+++ general_objects.py
@@ -2,8 +2,8 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
 from texture import load_texture
-from light import set_matte_properties, spotLoc
-from utils import draw_item_texture, textured_sphere, draw_solid_sphere, draw_quad_texture
+from light import set_matte_properties, set_shiny_properties, spotLoc
+from utils import draw_item_texture, draw_quad_texture, draw_item
 from globals import *
 
 
@@ -121,10 +121,193 @@ def draw_sun(center_x, center_y, center_z):
 
     glBindTexture(GL_TEXTURE_2D, 0)
     glDisable(GL_TEXTURE_2D)
-
     
 
+# A looping function that draws 20 fence pickets.
+def draw_fence(x, y, z):
+    glColor3f(1, 1, 1)
+    for i in range(NUM_PARTS):
+        draw_one_part_fence(x, y, z)
+        x += CHANGE
+    for i in range(NUM_PARTS):
+        draw_one_part_fence(x, y, z)
+        z -= CHANGE
+    for i in range(NUM_PARTS):
+        draw_one_part_fence(x, y, z)
+        x -= CHANGE
+
+# gets buttom left pos and draws the picket.
+def draw_one_part_fence(x, y, z):
+    fence_texture_id = load_texture("wood_texture.png")
+    
+    height_squar = 7
+    height_triangle = 2
+    width_on_z = 1
+    width_on_x = 1.5
+    vertices = [(x, y, z),
+                (x+width_on_x, y, z),
+                (x+width_on_x, y, z+width_on_z),
+                (x, y, z+width_on_z),
+                (x, y+height_squar, z),
+                (x+width_on_x, y+height_squar, z),
+                (x+width_on_x, y+height_squar, z+width_on_z),
+                (x, y+height_squar, z+width_on_z),
+                (x+width_on_x/2, y+height_squar+height_triangle, z+width_on_z/2)]
+    indices = [(0, 1, 2, 3),
+               (0, 1, 5, 4),
+               (1, 2, 6, 5),
+               (3, 2, 6, 7),
+               (3, 0, 4, 7), 
+               (4, 5, 8),
+               (5, 6, 8),
+               (6, 7, 8),
+               (4, 7, 8)]
+    
+    draw_item_texture(vertices, indices, fence_texture_id, 1)
+
+# calls seperate functions for setting up the sword in rock scene.
+def draw_rocks_and_sword(x,y,z):
+    # set rock to be matte
+    set_matte_properties()
+    draw_rock(x, y, z, ROCK_BASE, "rock_texture.png")
+    # set sword to be shiny
+    set_shiny_properties()
+    draw_sword(x+(1/2)*ROCK_BASE,y+ROCK_BASE,z-(1/2)*ROCK_BASE)
+    # set rock to be matte
+    set_matte_properties()
+    draw_rock(x+ROCK_BASE, y, z, (2/3)*ROCK_BASE, "rock2_texture.png")
+    draw_rock(x-(4/5)*ROCK_BASE, y, z-(1/2)*ROCK_BASE, (3/4)*ROCK_BASE, "rock2_texture.png")
+    draw_rock(x, y, z+(1/3)*ROCK_BASE, (1/3)*ROCK_BASE, "rock2_texture.png")
 
 
 
+# sets up parameters for a rock with give parameters
+def draw_rock(x, y, z, rock_base, texture_name):
+    rock_gap = int((1/4)*rock_base+1)
+    rock_texture_id = load_texture(texture_name)
+    vertices = [#base 0-3
+                (x, y, z),
+                (x, y, z-rock_base),
+                (x+rock_base, y, z),
+                (x+rock_base, y, z-rock_base),
+                #top 4-7
+                (x, y+rock_base, z),
+                (x, y+rock_base, z-rock_base),
+                (x+rock_base, y+rock_base, z),
+                (x+rock_base, y+rock_base, z-rock_base),
+                #sides
+                #move on x 8-11
+                (x+rock_gap, y+rock_gap, z+rock_gap),
+                (x+rock_base-rock_gap, y+rock_gap, z+rock_gap),
+                (x+rock_gap, y+rock_base-rock_gap, z+rock_gap),
+                (x+rock_base-rock_gap, y+rock_base-rock_gap, z+rock_gap),
+                #move on z 12-15
+                (x-rock_gap, y+rock_gap, z-rock_gap),
+                (x-rock_gap, y+rock_gap, z-rock_base+rock_gap),
+                (x-rock_gap, y+rock_base-rock_gap, z-rock_gap),
+                (x-rock_gap, y+rock_base-rock_gap, z-rock_base+rock_gap),
+                #move on x other side 16-19
+                (x+rock_gap, y+rock_gap, z-rock_base-rock_gap),
+                (x+rock_base-rock_gap, y+rock_gap, z-rock_base-rock_gap),
+                (x+rock_gap, y+rock_base-rock_gap, z-rock_base-rock_gap),
+                (x+2*rock_gap, y+rock_base-rock_gap, z-rock_base-rock_gap),
+                #move on z other side 20-23
+                (x+rock_base+rock_gap, y+rock_gap, z-rock_gap),
+                (x+rock_base+rock_gap, y+rock_gap, z-rock_base+rock_gap),
+                (x+rock_base+rock_gap, y+rock_base-rock_gap, z-rock_gap),
+                (x+rock_base+rock_gap, y+rock_base-rock_gap, z-rock_base+rock_gap),
+                #on top 24-27
+                (x+rock_gap, y+rock_base+rock_gap, z-rock_gap),
+                (x+rock_base-rock_gap, y+rock_base+rock_gap, z-rock_gap),
+                (x+rock_gap, y+rock_base+rock_gap, z-rock_base+rock_gap),
+                (x+rock_base-rock_gap, y+rock_base+rock_gap, z-rock_base-rock_gap)]
+    indices = [(0,1,13,12),
+               (0,4,14,12),
+               (4,5,15,14),
+               (1,5,15,13),
+               (12,13,15,14),
 
+               (1,3,17,16),
+               (3,7,19,17),
+               (7,5,18,19),
+               (5,1,16,18),
+               (16,17,19,18),
+
+               (2,3,21,20),
+               (3,7,23,21),
+               (7,6,22,23),
+               (6,2,20,22),
+               (20,21,23,22),
+
+               (0,2,9,8),
+               (2,6,11,9),
+               (6,4,10,11),
+               (4,0,8,10),
+               (8,9,11,10),
+
+               (4,5,26,24),
+               (5,7,27,26),
+               (7,6,25,27),
+               (6,4,24,25),
+               (24,25,27,26)]
+    
+    draw_item_texture(vertices, indices, rock_texture_id, 1)
+
+
+# Sets up parameters to draw a metallic sword in rocks.
+def draw_sword(x,y,z):
+    height_squar = 7
+    width_on_z = 0.5
+    width_on_x = 1.5
+    vertices = [(x, y, z),
+                (x+width_on_x, y, z),
+                (x+width_on_x, y, z+width_on_z),
+                (x, y, z+width_on_z),
+                (x, y+height_squar, z),
+                (x+width_on_x, y+height_squar, z),
+                (x+width_on_x, y+height_squar, z+width_on_z),
+                (x, y+height_squar, z+width_on_z),
+                (x+width_on_x/2, y+height_squar+width_on_z/2, z+width_on_z/2)]
+    indices = [((0, 1, 2, 3),0),
+               ((0, 1, 5, 4),0),
+               ((1, 2, 6, 5),0),
+               ((3, 2, 6, 7),0),
+               ((3, 0, 4, 7),0),
+               ((4, 5, 6, 7),0)]
+    
+
+    # draw the sworf accorsing to the parameters.
+    draw_item(vertices, indices, [(192/255,192/255,192/255)])
+
+
+
+    # draws the hilt at give coordinates
+    x, y, z = vertices[8]
+    draw_hilt(x, y, z, 0, 4)
+    draw_hilt(x, y+1, z, 90, 1.5)
+
+
+# draws a wooden hilt at input parameters.
+def draw_hilt(x,y,z,angle,len_z):
+    wood_texture_id = load_texture("wood_texture.png")
+    slices = 50
+    stacks = 50
+    len_x = 0.5
+    len_y = 0.5
+
+    glEnable(GL_TEXTURE_2D)
+    glBindTexture(GL_TEXTURE_2D, wood_texture_id)
+
+    glPushMatrix()
+    glTranslatef(x,y,z)
+    glRotate(angle,1,0,0)
+    glScalef(len_x, len_y, len_z)
+
+    quad = gluNewQuadric()
+    gluQuadricTexture(quad, GL_TRUE)
+    gluSphere(quad, 1, slices, stacks)
+
+    glPopMatrix()
+
+    glBindTexture(GL_TEXTURE_2D, 0)
+    glDisable(GL_TEXTURE_2D)
